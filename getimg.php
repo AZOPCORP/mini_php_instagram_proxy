106efae80590ac964fd3193a371d7fbd71c00617
AZOPCORP <azopcorp@gmail.com>
2015-11-16 13:20:37 +0100

    Update getimg.php

--- getimg.php
+++ getimg.php
@@ -3,20 +3,20 @@ session_start();//start a session
 $_SESSION['session_number'] = dechex(time());//name the session
 if (preg_match('#^https?://(?:[^.]+\.)*instagram\.com/#i', $_POST['uri'])) {//a litthe Regex to validate the instagram url
 
-$jsonURL ="http://api.instagram.com/publicapi/oembed/?url=".$_POST['uri'];//connect to instagram public API
+$jsonURL ="http://api.instagram.com/publicapi/oembed/?url=".$_POST['uri'];//link to instagram public API
 
-$json = file_get_contents($jsonURL); 
-$data = json_decode($json);
-$tempfile = "imagetemp.php?id=".$_SESSION['session_number'];
-
-
-$_SESSION['link'] = $data->{"thumbnail_url"};
+$json = file_get_contents($jsonURL); //connect to the API
+$data = json_decode($json);//decode the JSON and assign to a variable
+$tempfile = "imagetemp.php?id=".$_SESSION['session_number'];//create temporary link to our image reffers to imagetemp.php
 
 
+$_SESSION['link'] = $data->{"thumbnail_url"}; //store the remote image url in a session variable so we can pass it to imagetemp.php
 
-echo '<div id="link">'.$tempfile.'</div>';
 
-echo '<img id="source" src="'.$tempfile.'" />';
+
+echo '<div id="link">'.$tempfile.'</div>'; // 
+
+echo '<img id="source" src="'.$tempfile.'" />';//echo our proxy image
 
 
     
